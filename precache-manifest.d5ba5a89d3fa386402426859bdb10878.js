9b040c38e25692f32a01f760979c7cc30ae6ba5e
ermias kidane <hadeklte21@gmail.com>
2021-01-27 20:30:56 +0000

    Updates

---
+++ precache-manifest.d5ba5a89d3fa386402426859bdb10878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e987e1dc4c661c31ba8b7b70c70f3d75",
+    "url": "/covid-19-app/index.html"
+  },
+  {
+    "revision": "e5821dff1ed87371c008",
+    "url": "/covid-19-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "66e8d52353e1e122816c",
+    "url": "/covid-19-app/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "e5821dff1ed87371c008",
+    "url": "/covid-19-app/static/js/2.bd00fffe.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-app/static/js/2.bd00fffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e8d52353e1e122816c",
+    "url": "/covid-19-app/static/js/main.44ce171e.chunk.js"
+  },
+  {
+    "revision": "91a457564a798b12bc69",
+    "url": "/covid-19-app/static/js/runtime-main.1ad87898.js"
+  }
+]);
